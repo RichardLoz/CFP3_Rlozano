9fe29543ad3a617342f512b5bcad2550ca496f46
RichardLoz <lozanorichard32@gmail.com>
2023-11-02 20:38:49 -0300

    Practica For-While

--- 01_Tecnicas_de_Programacion/Clase_16/for.py
+++ 01_Tecnicas_de_Programacion/Clase_16/for.py
@@ -41,5 +41,61 @@ meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio", "Agosto", "Se
 #     print(i)
 
 # RECORRER DE MANERA INVERSA
-for i in range(11,1,-1):
-    print(i)
+# for i in range(11,1,-1):
+#     print(i)
+
+#TODO: EJECICIO:
+# Escribir un programa que genere una lista de numeros pares del 0 al 10 (ambos inclusive), para esto debemos utilizar un ciclo FOR y el metodo append. Finalmente imprimir la lista de numeros por pantalla.
+lista_pares = []
+lista_impares = []
+# for i in range (0,12,2):
+#     print(i)
+#     lista.append(i)
+# print(lista)
+
+#TODO: Lista de numeros Pares - Impares
+# lista_pares = []
+# lista_impares = []
+# for i in range(11):
+#     if i % 2 == 0:
+#         lista_pares.append(i)
+#     else:
+#         lista_impares.append(i)
+# print(lista_pares)
+# print(lista_impares)
+
+#TODO: Dada una lista numeros=[1,2,3,4,5] hacer un programa que recorra la lista y vaya eliminando los elementos de la lista uno a uno y los muestre por pantalla hasta que la liste quede vacia
+#TODO: DESCENDENTE
+# numeros = [1,2,3,4,5]
+# for i in range(len(numeros)-1, -1, -1):
+#     numero_eliminado = numeros.pop(i)
+#     print(f"Se elimino el numero {numero_eliminado}")
+# print(f"La lista quedo vacia {numeros}")
+
+# #TODO: ASCENDENTE
+# numeros = [1,2,3,4,5]
+# for _ in range(len(numeros)):
+#     print(numeros)
+#     numero_eliminado = numeros.pop(0)
+#     print(numeros)
+#     print(f"Se elimino el numero {numero_eliminado}")
+# print(f"La lista quedo vacia {numeros}")
+
+
+
+
+#TODO: Crear un programa que almacene dos listas: usuarios y contraseñas y solicite al ingresante los datos de login (usuario y contraseña) solo se admiten 3 intentos de inicio de sesión.
+usuarios = ["Bryan", "Tomas", "Ronal", "Santiago", "Roberto"]
+contraseñas = ["pepito", "123", "45", "contraseña","pepito1"]
+intentos = 3
+while intentos > 0 :
+    usuario_ingresado = input("Ingrese su usuario: ").capitalize()
+    contraseña_ingresada = input("Ingrese su contraseña: ").lower()
+    if usuario_ingresado in usuarios and contraseña_ingresada == contraseñas[usuarios.index(usuario_ingresado)]:
+        print(f"Inicio de sesión correcto. Bienvenido {usuario_ingresado}")
+        break
+    else:
+        print("Credenciales incorrectas, intente nuevamente")
+        intentos -= 1
+if intentos == 0:
+    print("Su cuenta fue bloqueada, comunicarse con el departamento de Sistemas")
