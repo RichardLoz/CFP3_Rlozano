72e421edc858e0c51138a20cb1aa0f77c33ef9e9
carlosmonsa <143462859+carlosmonsa@users.noreply.github.com>
2023-11-23 20:13:38 -0300

    Examen.py

---
+++ 01_Tecnicas_de_Programacion/Z_Parcial/Roberto Zare/examen.py
@@ -0,0 +1,35 @@
+#2-	Dada una lista de palabras, ordénala alfabéticamente sin usar funciones de ordenamiento integradas.
+
+lista = ["Elefante","Banco","Campo","Dedo","Avion"]
+lista.sort()
+print(lista)
+
+
+
+
+
+
+
+# #5-	El Ministerio de Salud les pide que realicen un algoritmo que pueda clasificar temperaturas en tres categorías: "Baja", "Media" y "Alta". Tu tarea es desarrollar un programa que solicite al usuario ingresar temperaturas y las clasifique según los siguientes criterios:
+# Baja: Temperaturas menores o iguales a 10 grados Celsius.
+# Media: Temperaturas mayores a 10 grados y menores o iguales a 25 grados Celsius.
+# Alta: Temperaturas mayores a 25 grados Celsius.
+
+# Además, se desea que el programa almacene las temperaturas en listas separadas para cada categoría.
+# Al finalizar imprimir por pantalla las temperaturas correspondientes a cada categoría. 
+
+
+
+
+
+
+
+
+
+
+
+#1-	Solicita al usuario que ingrese una contraseña. Verifica si la contraseña cumple con los siguientes criterios: al menos 8 caracteres, contiene al menos una letra mayúscula, una letra minúscula y un número.
+
+# contraseña = input("Ingrese una contraseña por favor: ")
+
+
