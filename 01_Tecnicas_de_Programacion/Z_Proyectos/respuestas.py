df8908b984d2b7a19d093f0aaa92e42c40c7a61e
RichardLoz <lozanorichard32@gmail.com>
2023-11-28 17:46:08 -0300

    Solucion parcial

--- 01_Tecnicas_de_Programacion/Z_Proyectos/respuestas.py
+++ 01_Tecnicas_de_Programacion/Z_Proyectos/respuestas.py
@@ -1,107 +1,94 @@
-# # Solicitar la contraseña al usuario
-# contrasena_ingresada = input("Ingrese su contraseña: ")
 
-# # Verificar la longitud de la contraseña
-# if len(contrasena_ingresada) < 8:
-#     print("La contraseña debe tener al menos 8 caracteres.")
-# else:
-#     # Verificar si contiene al menos una letra mayúscula
-#     tiene_mayuscula = False
-#     for caracter in contrasena_ingresada:
+#TODO: 1-
+
+# Solicitamos contraseña al usuario
+# contraseña_ingresada = input("Ingrese su contraseña: ")
+# # Pepito123
+# #Verificar si la contraseña es mayor a 8 caracteres
+# if len(contraseña_ingresada) < 8 :
+#     print("La contraseña debe tener minimo 8 caracteres")
+# else: 
+#     #Verificar si tiene mayuscula
+#     for caracter in contraseña_ingresada :
+#         contiene_mayuscula   = False
 #         if 'A' <= caracter <= 'Z':
-#             tiene_mayuscula = True
+#             contiene_mayuscula = True
 #             break
-
-#     # Verificar si contiene al menos una letra minúscula
-#     tiene_minuscula = False
-#     for caracter in contrasena_ingresada:
+#     #Verificar si tiene minuscula
+#     for caracter in contraseña_ingresada:
+#         contiene_minuscula = False
 #         if 'a' <= caracter <= 'z':
-#             tiene_minuscula = True
+#             contiene_minuscula = True
 #             break
-
-#     # Verificar si contiene al menos un número
-#     tiene_numero = False
-#     for caracter in contrasena_ingresada:
+#     #Verificar si tiene numero
+#     for caracter in contraseña_ingresada:
+#         contiene_numero = False
 #         if '0' <= caracter <= '9':
-#             tiene_numero = True
+#             contiene_numero = True
 #             break
-
-#     # Mostrar el resultado
-#     if tiene_mayuscula and tiene_minuscula and tiene_numero:
-#         print("La contraseña es válida.")
+#     if contiene_mayuscula and contiene_minuscula and contiene_numero:
+#         print("La contraseña es valida")
 #     else:
-#         print("La contraseña no cumple con los criterios especificados.")
+#         print("Contraseña no valida")
 
-#2 Ordenar la lista de palabras
-lista = ["arbol", "vegetal", "dinosarusio", "zapato", "leon"]
+
+#TODO: 2- Ordenando lista alfabeticamente
+lista = ["arbol", "javier", "fernando", "victor", "manuel", "bastian", "ana", "roberto"]
 
 n = len(lista)
 
 for i in range(n - 1):
-    for j in range(0, n - i - 1):
+    for j in range(n - i - 1):
         if lista[j] > lista[j + 1]:
             lista[j], lista[j + 1] = lista[j + 1], lista[j]
 print(lista)
 
 
-#3: Comprobar si una palabra es palindromo
-# palabra = input("Ingrese una palabra: ")
-# palabra_reves = palabra[::-1]
-# if palabra == palabra_reves:
-#     print("La palabra es palindromo.")
+
+
+
+#TODO: 3-
+# palabra = input("Ingrese la palabra: ")
+# if palabra == palabra_invertida:
+#     print("Es palindromo")
 # else:
-#     print("La palabra no es palindromo.")
+#     print("No es palindromo")
 
 
+#TODO: 4-
+#Solicitamos codigo verificador
+#codigo_verificador = input("Ingrese su codigo verificador: ").upper()
+#Solicitamos el valor de la entrada
+# precio = int(input("Ingrese el valor de su entrada: "))
+# #Clasificamos el tipo de entrada
+# # if codigo_verificador[-1] == "X" and precio > 50000:
+# #TODO: La funcion endwith en Python se utiliza para verificar si una cadena de texto termina en una subcadena especifica. Retorna True o False si la cadena termina en la subcadena proporcionada.
+# if codigo_verificador.endswith("X") and precio > 50000:
+#     categoria = "Aventura"
+# elif codigo_verificador[-1] == "Y" and 20000 <= precio <= 50000:
+#     categoria = "Familiar"
+# else: 
+#     categoria = "General"
+# #Mostramos la categoria de la entrada
+# print(f"La entrada pertenece a la categoria {categoria}")
 
 
+#TODO: 5-
+# temperaturas = int(input("Ingrese los registros de temperatura:  ")) 
+# baja = []
+# media = []
+# alta = []
+# for i in range(temperaturas):
+#     temperatura = int(input("Ingrese la temperatura: "))
+#     if temperatura <= 10:
+#         baja.append(temperatura)
+#     elif 10 < temperatura <= 25:
+#         media.append(temperatura)
+#     else: 
+#         alta.append(temperatura)
 
+# print("Temperaturas bajas: ", baja)
+# print("Temperaturas media: ", media)
+# print("Temperaturas alta: ", alta)
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#TODO: 4
-
-codigo = ("Ingrese su codigo verificado: ")
-# FSDHJSJFSFBJX
-#234235235235X
-#HFGBFG35325X
-precio = int(input("Ingres el valor de su entrada: "))
-
-
-if codigo[-1] == "X" and precio > 50000:
-    print("Aventura")
-    
-    
-    
-    
-#5
-baja = []
-temperaturas = int(input("Ingrese la temperaturas: ")) ==> 10
-for i in range(temperaturas):
-    if i >= 10:
-        baja.append(i)
-    
