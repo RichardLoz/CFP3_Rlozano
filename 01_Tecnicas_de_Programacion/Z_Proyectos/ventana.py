0780c31031b877567834263bdba01b49a16fd3ab
RichardLoz <lozanorichard32@gmail.com>
2024-02-19 19:03:54 -0300

    Clase 01 DB

---
+++ 01_Tecnicas_de_Programacion/Z_Proyectos/ventana.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+class App():
+    def __init__(self):
+        ventana = Tk()
+        ventana.title("Generador de Password")
+        ventana.geometry("400x400")
+        ventana.configure(bg="blue")
+        
+        
+        ventana.mainloop()
+        
+objeto_ventana = App()
+        
