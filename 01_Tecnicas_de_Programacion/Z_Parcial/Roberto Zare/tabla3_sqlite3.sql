af48ecfcfecdbc8585e23d40072f9e356e0a989f
carlosmonsa <143462859+carlosmonsa@users.noreply.github.com>
2024-03-21 21:30:51 -0300

    Entrega Parcial Zare

---
+++ 01_Tecnicas_de_Programacion/Z_Parcial/Roberto Zare/tabla3_sqlite3.sql
@@ -0,0 +1,36 @@
+CREATE table if not EXISTS Cliente (
+	ID_Cliente INTEGER PRIMARY key AUTOINCREMENT,
+	Nombre text,
+	Apellido text,
+	Email text,
+	Telefono text 
+	);
+
+CREATE table if not EXISTS Reclamo (
+	ID_Reclamo INTEGER PRIMARY key AUTOINCREMENT,
+	Fecha datetime,
+	hora datetime,
+	Descripcion_Reclamo text,
+	ID_Cliente INTEGER,
+	Estado_del_Reclamo text,
+	FOREIGN KEY(ID_Cliente)REFERENCES Cliente (ID_Cliente)
+	);
+	
+	
+CREATE table if not EXISTS Departamento (
+	ID_Departamento INTEGER PRIMARY key AUTOINCREMENT,
+	Nombre text
+	);
+	
+	insert into Departamento(nombre) VALUES ('soporte_tecnico');
+	insert into Departamento(Nombre)VALUES ('Facturacion');
+	insert into Departamento(Nombre) VALUES ('Servicio_al_Cliente');
+	
+
+CREATE table if not EXISTS Empleado (
+	ID_Empleado INTEGER PRIMARY key AUTOINCREMENT,
+	Nombre text,
+	Apellido text,
+	ID_Departamento INTEGER,
+	FOREIGN key(ID_Departamento)REFERENCES Departamento(ID_Departamento)
+	);
---
+++ 01_Tecnicas_de_Programacion/Z_Parcial/Roberto Zare/tabla5_sqlite3.sql
@@ -0,0 +1,22 @@
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('3/4/2024', '2:40 PM', 'Laceration with foreign body, right foot, subs encntr', 8, 'En trámite');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('7/28/2023', '5:35 PM', 'Sltr-haris Type I physeal fx phalanx of unsp toe, 7thB', 14, 'En trámite');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('8/16/2023', '5:05 AM', 'Chronic cluster headache', 7, 'En evaluación');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('10/14/2023', '9:19 AM', 'Type 3 fracture of sacrum, subs for fx w nonunion', 1, 'En espera');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('8/14/2023', '11:09 AM', 'Milt op involving fragments from munitions, civilian, subs', 15, 'En análisis');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('12/21/2023', '6:01 AM', 'Pathological fracture in oth disease, pelvis, init for fx', 16, 'En seguimiento');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('5/21/2023', '10:48 PM', 'Other chondrocalcinosis, unspecified site', 17, 'Cancelado');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('6/12/2023', '1:10 PM', 'Toxic effect of corrosive organic compnd, self-harm, sequela', 14, 'En proceso');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('3/4/2024', '6:32 AM', 'Neoplasm of uncertain behavior of right kidney', 3, 'En proceso');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('11/16/2023', '2:43 PM', 'Bitten by alligator, initial encounter', 16, 'En trámite');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('10/28/2023', '11:36 PM', 'Lacerat msl/tnd lng flxr msl of toe at ank/ft level, r foot', 12, 'En seguimiento');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('3/22/2023', '12:06 AM', 'Sltr-haris Type IV physeal fx upper end radius, right arm', 14, 'Resuelto');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('11/16/2023', '1:19 AM', 'Unspecified asthma with status asthmaticus', 4, 'Cancelado');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('8/4/2023', '10:38 PM', 'Oth accident on gliding-type pedestrian conveyance, subs', 12, 'Resuelto');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('1/25/2024', '11:39 AM', 'Maternal care for Anti-A sensitization, unsp trimester, unsp', 14, 'En proceso');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('6/30/2023', '3:51 AM', 'Terrorism involving suicide bomber', 1, 'En seguimiento');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('10/5/2023', '1:29 AM', 'War operations involving explosion of marine mine, milt', 2, 'En revisión');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('1/24/2024', '5:51 AM', 'Gastric contents in esophagus causing oth injury, init', 12, 'Pendiente de revisión');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('2/19/2024', '8:45 AM', 'Displ oblique fx shaft of unsp rad, 7thK', 12, 'En trámite');
+insert into Reclamo (fecha, hora, Descripcion_Reclamo, ID_cliente, Estado_del_reclamo) values ('1/5/2024', '9:38 PM', 'Inj extensor musc/fasc/tend thmb at wrs/hnd lv, sequela', 6, 'En espera');
+
+drop TABLE Departamento
