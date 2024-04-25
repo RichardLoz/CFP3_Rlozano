bf97ed58b8b37eb41d2f6a85d1d82d33954dae06
RichardLoz <lozanorichard32@gmail.com>
2024-04-24 21:26:03 -0300

    Practica FUnciones III

--- 04_POO/Clase_05/Practica_24_04.py
+++ 04_POO/Clase_05/Practica_24_04.py
@@ -32,5 +32,47 @@ def suma_bucle():
         resultado_suma = suma(num1, num2)
         print(f"La suma entre {num1} y {num2} es {resultado_suma}")
         
+#suma_bucle()
+
+
+#TODO: ENUMERATE: Es una funcion en Python que se utiliza para enumerar elementos de una secuencia (lista, tuplas, etc), mientras los recorre en un bucle. Enumerate toma dos parametros: la secuencia que se quiere sumar y opcionalmente el indice inicial. Devuelve un objeto enumerado que produce tuplas que contienen un contador y un elemento de la secuencia.
+
+# frutas = ["manzana", "banana", "pera", "anana", "naranja"]
+
+# for indice,fruta in enumerate (frutas, start = 1):
+#     print(f"Fruta {indice}: {fruta}")
+
+
+#TODO: Lo que se solicita es que se guarden todos los resultados de las sumas y mostrarlos al salir del bucle
+
+def suma(num1,num2):
+    resultado = num1 + num2
+    return resultado
+
+def suma_bucle():
+    resultados = []
+    total = 0
+    while True:
+        num1 = int(input("Ingrese el primer valor (Ingrese un numero negativo para salir: )"))
+        if num1 < 0:
+            print("Usted esta saliendo el programa.....")
+            break
+        num2 = int(input("Ingrese el segundo valor: "))
+        resultado_suma = suma(num1, num2)
+        resultados.append(resultado_suma)
+        print(f"La suma entre {num1} y {num2} es {resultado_suma}") 
+    #Mostrar Resultados
+    print(f"El resultado de las sumas es: {sum(resultados)} ")
+    for indice, resultado in enumerate(resultados, start=1):
+        print(f"Suma {indice}: {resultado}")
+        
+#Pendiente: PROMEDIO
+        
+    
+        
 suma_bucle()
+
+
+
+
         
