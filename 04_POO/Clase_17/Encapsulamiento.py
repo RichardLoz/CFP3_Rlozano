791d4310038bcaf7b0e42686df6a6ec42fb681b6
RichardLoz <lozanorichard32@gmail.com>
2024-06-19 18:59:54 -0300

    TP con nota

--- 04_POO/Clase_17/Encapsulamiento.py
+++ 04_POO/Clase_17/Encapsulamiento.py
@@ -115,3 +115,166 @@ class Usuario:
 # Un método listar_estudiantes() que devuelva una cadena con todos los nombres de los estudiantes inscritos, separados por comas.
 # Un método __str__ para representar el objeto como una cadena que muestre el nombre del curso y el nombre del instructor.
 
+# class Producto:
+#     def __init__(self, nombre, precio, stock):
+#         self.__nombre = nombre
+#         self.__precio = precio
+#         self.__stock = stock
+
+#     # Getter y Setter para nombre
+#     def get_nombre(self):
+#         return self.__nombre
+
+#     def set_nombre(self, nombre):
+#         self.__nombre = nombre
+
+#     # Getter y Setter para precio
+#     def get_precio(self):
+#         return self.__precio
+
+#     def set_precio(self, precio):
+#         if precio > 0:
+#             self.__precio = precio
+#         else:
+#             print("El precio debe ser un número positivo.")
+
+#     # Getter y Setter para stock
+#     def get_stock(self):
+#         return self.__stock
+
+#     def set_stock(self, stock):
+#         if isinstance(stock, int) and stock >= 0:
+#             self.__stock = stock
+#         else:
+#             print("El stock debe ser un número entero no negativo.")
+
+#     # Método __str__ para representación en cadena
+#     def __str__(self):
+#         return f'Producto: {self.__nombre}, Precio: {self.__precio}, Stock: {self.__stock}'
+
+# # Uso de la clase Producto
+# p1 = Producto("Laptop", 1500.0, 5)
+# print(p1)  # Producto: Laptop, Precio: 1500.0, Stock: 5
+
+# p1.set_precio(1400.0)
+# print(p1.get_precio())  # 1400.0
+
+# p1.set_stock(10)
+# print(p1.get_stock())  # 10
+
+
+
+
+# class Empleado:
+#     def __init__(self, nombre, salario, departamento):
+#         self.__nombre = nombre
+#         self.__salario = salario
+#         self.__departamento = departamento
+
+#     # Getter y Setter para nombre
+#     def get_nombre(self):
+#         return self.__nombre
+
+#     def set_nombre(self, nombre):
+#         self.__nombre = nombre
+
+#     # Getter y Setter para salario
+#     def get_salario(self):
+#         return self.__salario
+
+#     def set_salario(self, salario):
+#         if salario > 0:
+#             self.__salario = salario
+#         else:
+#             print("El salario debe ser un número positivo.")
+
+#     # Getter 
+#     # y Setter para departamento
+#     def get_departamento(self):
+#         return self.__departamento
+
+#     def set_departamento(self, departamento):
+#         self.__departamento = departamento
+
+#     # Método para aumentar el salario
+#     def aumentar_salario(self, porcentaje):
+#         if porcentaje > 0:
+#             self.__salario += self.__salario * (porcentaje / 100)
+#         else:
+#             print("El porcentaje debe ser positivo.")
+
+#     # Método __str__ para representación en cadena
+#     def __str__(self):
+#         return f'Empleado: {self.__nombre}, Salario: {self.__salario}, Departamento: {self.__departamento}'
+
+# # Uso de la clase Empleado
+# e1 = Empleado("Juan Perez", 3000.0, "IT")
+# print(e1)  # Empleado: Juan Perez, Salario: 3000.0, Departamento: IT
+
+# e1.aumentar_salario(10)
+# print(e1.get_salario())  # 3300.0
+
+# e1.set_departamento("HR")
+# print(e1.get_departamento())  # HR
+
+
+
+
+class Curso:
+    def __init__(self, nombre_curso, instructor):
+        self.__nombre_curso = nombre_curso
+        self.__instructor = instructor
+        self.__estudiantes = []
+
+    # Getter y Setter para nombre_curso
+    def get_nombre_curso(self):
+        return self.__nombre_curso
+
+    def set_nombre_curso(self, nombre_curso):
+        self.__nombre_curso = nombre_curso
+
+    # Getter y Setter para instructor
+    def get_instructor(self):
+        return self.__instructor
+
+    def set_instructor(self, instructor):
+        self.__instructor = instructor
+
+    # Getter para la lista de estudiantes
+    def get_estudiantes(self):
+        return self.__estudiantes
+
+    # Método para agregar un estudiante
+    def agregar_estudiante(self, estudiante):
+        self.__estudiantes.append(estudiante)
+
+    # Método para remover un estudiante
+    def remover_estudiante(self, estudiante):
+        if estudiante in self.__estudiantes:
+            self.__estudiantes.remove(estudiante)
+        else:
+            print(f"El estudiante {estudiante} no está inscrito en el curso.")
+
+    # Método para listar los estudiantes
+    def listar_estudiantes(self):
+        return ", ".join(self.__estudiantes)
+
+    # Método __str__ para representación en cadena
+    def __str__(self):
+        return f'Curso: {self.__nombre_curso}, Instructor: {self.__instructor}'
+
+# Uso de la clase Curso
+curso = Curso("Base de Datos", "Prof. Lozano")
+print(curso)  # Curso: Matemáticas, Instructor: Prof. Gomez
+
+curso.agregar_estudiante("Milagros")
+curso.agregar_estudiante("Jesica")
+print(curso.get_estudiantes())  
+
+curso.remover_estudiante("Bryan")
+curso.agregar_estudiante("Richard")
+curso.agregar_estudiante("Tomas")
+print(curso.listar_estudiantes())  # Bob
+
+curso.set_nombre_curso("Programacion Python")
+print(curso.get_nombre_curso())  # Matemáticas Avanzadas
---
+++ 04_POO/Clase_18/TP.py
@@ -0,0 +1,38 @@
+
+# 1- Clase Biblioteca
+# Crea una clase Biblioteca que represente una colección de libros. La clase debe tener los siguientes atributos privados:
+
+# __nombre_biblioteca: El nombre de la biblioteca (cadena de texto).
+# __direccion: La dirección de la biblioteca (cadena de texto).
+# __libros: Una lista que contenga los títulos de los libros disponibles en la biblioteca (cadena de texto).
+# Implementa los siguientes métodos:
+
+# Métodos getter y setter para nombre_biblioteca.
+# Métodos getter y setter para direccion.
+# Un método getter para obtener la lista de libros get_libros().
+# Un método agregar_libro(libro) que añada un libro a la lista de libros.
+# Un método remover_libro(libro) que elimine un libro de la lista de libros si existe en la lista.
+# Un método buscar_libro(libro) que devuelva True si el libro está en la lista y False en caso contrario.
+# Un método listar_libros() que devuelva una cadena con todos los títulos de los libros disponibles, separados por comas.
+# Un método reemplazar_libro(libro_viejo, libro_nuevo) que reemplace un libro viejo por uno nuevo si el libro viejo existe en la lista.
+# Un método __str__ para representar el objeto como una cadena que muestre el nombre de la biblioteca y su dirección.
+
+
+#=============================================================================================
+
+#2- Clase EquipoDeFutbol
+# Crea una clase EquipoDeFutbol que represente un equipo de fútbol. La clase debe tener los siguientes atributos privados:
+
+# __nombre_equipo: El nombre del equipo (cadena de texto).
+# __entrenador: El nombre del entrenador del equipo (cadena de texto).
+# __jugadores: Una lista que contenga los nombres de los jugadores en el equipo (cadena de texto).
+# Implementa los siguientes métodos:
+
+# Métodos getter y setter para nombre_equipo.
+# Métodos getter y setter para entrenador.
+# Un método getter para obtener la lista de jugadores get_jugadores().
+# Un método agregar_jugador(jugador) que añada un jugador a la lista de jugadores.
+# Un método remover_jugador(jugador) que elimine un jugador de la lista de jugadores si existe en la lista.
+# Un método listar_jugadores() que devuelva una cadena con todos los nombres de los jugadores en el equipo, separados por comas.
+# Un método cambiar_capitan(capitan_nuevo) que reemplace el capitán actual por uno nuevo si el capitán nuevo está en la lista de jugadores.
+# Un método __str__ para representar el objeto como una cadena que muestre el nombre del equipo y el nombre del entrenador.
